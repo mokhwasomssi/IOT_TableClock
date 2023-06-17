da1b9c10fb48d4290088528e309ac0559844f3bb
Eunhye Seok <mokhwasomssi@gmail.com>
2023-06-18 05:14:22 +0900

    feat : 버스 도착 정보 수신 (미검증)

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # widget_tableclock
 실용성있는 위젯을 추가한 IOT 탁상시계
 
+## ESP32 dev board
+- [ESP32-C3-DevKitM-1](https://docs.espressif.com/projects/esp-idf/en/latest/esp32c3/hw-reference/esp32c3/user-guide-devkitm-1.html)
+- [ESP32­-C3­-MINI-­1](https://www.espressif.com/sites/default/files/documentation/esp32-c3-mini-1_datasheet_en.pdf)
+- [ESP32-C3FN4](https://www.espressif.com/sites/default/files/documentation/esp32-c3_datasheet_en.pdf)
+  
+
 ## 설명서
 
 - [시스템도](https://whimsical.com/wiget-tableclock-EQHfxn7x5JfxGt9CBQZhHa)
@@ -12,6 +18,7 @@
     - CST816S 라이브러리
       - 헤더, 소스파일 직접 포함 [fbiego/CST816S](https://github.com/fbiego/CST816S)
       - `uint8_t i2c_read(uint16_t addr, uint8_t reg_addr, uint8_t * reg_data, size_t length)`로 수정함
+    - [tinyxml2](https://github.com/leethomason/tinyxml2) : XML parser
 - ESP32-C3-DEVKITM-1_V1  
   - GPIO 8, 9 사용 X  
 - 1.28 inch round Touch Display  
@@ -34,8 +41,18 @@
   #define TOUCH_INT 6
   #define TOUCH_RST 7
   ```
+  - API
+    - [Weather API](https://openweathermap.org/)
+
 
 ## 참고자료
-- https://github.com/Makerfabs/ESP32-S3-Round-SPI-TFT-with-Touch-1.28
+- 원형 디스플레이
+  - https://github.com/Makerfabs/ESP32-S3-Round-SPI-TFT-with-Touch-1.28
+- 현재 날짜 및 시각
+  - https://randomnerdtutorials.com/esp32-date-time-ntp-client-server-arduino/
 - 
-- 
+
+
+## 예상질문
+- 스마트워치하고 차이점
+- 스마트미러하고 차이점
--- widget_tableclock/widget_tableclock.ino
+++ widget_tableclock/widget_tableclock.ino
@@ -1,6 +1,17 @@
 #include <string.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 #include "EEPROM.h"
+#include "time.h"
+#include <tinyxml2.h>
+
+// 공공데이터포털 개인 API 인증키
+#define PUBLIC_DATA_API_KEY "HHec4G%2FFjjMjQIfzZa3yfZuItK93BQh%2BC%2FwkITl%2FCu8X3h%2BAjlF74glKicSnEN%2BVeZEOvstt07Zz%2Be%2BvmAlFVQ%3D%3D" 
+
+// OpenWeather API 키
+#define OPENWEATHER_API_KET "8d27c680ce67b6ffebcf09d005cdd444"
 
 #define EEPROM_SIZE 256
 
@@ -19,8 +30,203 @@ typedef struct
     char stock_num[32];
 } USER_DATA;
 
+typedef struct
+{
+    int busStationName;
+    int stationId;
+    int routeName;
+    int routeId;
+    int staOrder;
+
+    int routeName_temp;
+
+    int locationNo1;
+    int predictTime1;
+    int locationNo2;
+    int predictTime2;
+} BUS;
+
 TABLECLOCK_STATE my_state = WIDGET_NULL;
 USER_DATA my_data;
+BUS my_bus;
+
+// NTP server to request epoch time
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 32400; // +9:00 (9hours*60mins*60seconds)
+const int daylightOffset_sec = 0;
+
+wifi_mode_t esp_wifi_mode = WIFI_MODE_NULL;
+WiFiClient mySocket;
+HTTPClient myHTTP;
+DynamicJsonDocument doc(2048);
+
+using namespace tinyxml2;
+// char * xmlDoc = "<root><body><element>7</element><element2>8</element2>></body></root>";
+// char * xmlDoc = "<response><comMsgHeader/><msgHeader><queryTime>2023-06-18 01:25:40.486</queryTime><resultCode>0</resultCode><resultMessage>정상적으로 처리되었습니다.</resultMessage></msgHeader><msgBody><busStationList><centerYn>N</centerYn><stationId>228000162</stationId></busStationList></msgBody></response>";
+
+char xmlDoc[4096];
+
+void getTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%a, %b %d %Y %H:%M:%S");
+}
+
+void getWeather()
+{
+    myHTTP.begin(mySocket, "http://api.openweathermap.org/data/2.5/weather?q=Yongin&appid=8d27c680ce67b6ffebcf09d005cdd444");
+
+    if(myHTTP.GET() == HTTP_CODE_OK)
+    {
+    printf("weather Received! \r\n");
+    deserializeJson(doc, myHTTP.getString());
+    const char *cityStr = doc["name"];
+    const char *currWeather = doc["weather"][0]["main"];
+    float temp = (float)(doc["main"]["temp"]) - 273.0;
+    printf("%s의 날씨 : %s, 온도 : %f \r\n", cityStr, currWeather, temp);
+    }
+    else
+    {
+        printf("Server error! \r\n");
+    }
+    myHTTP.end();
+}
+
+void getBusStationId() // getBusStationList Operation
+{
+    char buffer[512];
+    snprintf(buffer, 512, "http://apis.data.go.kr/6410000/busstationservice/getBusStationList?serviceKey=%s&keyword=%d", PUBLIC_DATA_API_KEY, my_bus.busStationName);
+
+    myHTTP.begin(mySocket, buffer);
+
+    if(myHTTP.GET() == HTTP_CODE_OK)
+    {
+        printf("BusStationListResponse \r\n");
+        strcpy(xmlDoc, myHTTP.getString().c_str());
+        printf("%s \r\n", xmlDoc);
+    }
+    else
+    {
+        printf("BusStationListResponse error! \r\n");
+    }
+
+    XMLDocument xmlDocument;
+    if(xmlDocument.Parse(xmlDoc)!= XML_SUCCESS)
+    {
+        Serial.println("BusStationListResponse error parsing");  
+        return;
+    };
+
+    XMLNode * root = xmlDocument.RootElement();
+    XMLElement * element = root->FirstChildElement("msgBody")->FirstChildElement("busStationList")->FirstChildElement("stationId");
+
+    element->QueryIntText(&my_bus.stationId);	
+    Serial.print("BusStationId : ");
+    Serial.println(my_bus.stationId);
+
+    myHTTP.end();
+}
+
+void getStaOrder() // getBusStationViaRouteList Operation, Route : 노선 번호(=버스 번호)
+{
+    char buffer[512];
+    snprintf(buffer, 512, "http://apis.data.go.kr/6410000/busstationservice/getBusStationViaRouteList?serviceKey=%s&stationId=%d", PUBLIC_DATA_API_KEY, my_bus.stationId);
+
+    myHTTP.begin(mySocket, buffer);
+
+    if(myHTTP.GET() == HTTP_CODE_OK)
+    {
+        printf("getBusStationViaRouteList \r\n");
+        strcpy(xmlDoc, myHTTP.getString().c_str());
+        printf("%s \r\n", xmlDoc);
+    }
+    else
+    {
+        printf("getBusStationViaRouteList error! \r\n");
+    }
+
+    XMLDocument xmlDocument;
+    if(xmlDocument.Parse(xmlDoc)!= XML_SUCCESS)
+    {
+        Serial.println("getBusStationViaRouteList error parsing");  
+        return;
+    };
+
+    XMLNode* root = xmlDocument.RootElement();
+    XMLElement* element = root->FirstChildElement("msgBody")->FirstChildElement("busStationList")->FirstChildElement("routeName");
+
+    for(XMLElement* routeName = element; routeName != NULL; routeName->NextSiblingElement())
+    {
+        routeName->QueryIntText(&my_bus.routeName_temp);
+
+        if(my_bus.routeName_temp == my_bus.routeName)
+        {
+            XMLElement* ele1 = root->FirstChildElement("msgBody")->FirstChildElement("busStationList")->FirstChildElement("routeId");
+            ele1->QueryIntText(&my_bus.routeId);
+
+            XMLElement* ele2 = root->FirstChildElement("msgBody")->FirstChildElement("busStationList")->FirstChildElement("staOrder");
+            ele2->QueryIntText(&my_bus.staOrder);
+            
+            break;
+        }
+    }
+
+    Serial.print("Bus routeId : ");
+    Serial.println(my_bus.routeId);
+    Serial.print("Bus staOrder : ");
+    Serial.println(my_bus.staOrder);
+
+    myHTTP.end();
+}
+
+void getBusArrival() // getBusArrivalItem Operation
+{
+    char buffer[512];
+    snprintf(buffer, 512, "http://apis.data.go.kr/6410000/busstationservice/getBusArrivalItem?serviceKey=%s&stationId=%d&routeId=%d&staOrder=%d", PUBLIC_DATA_API_KEY, my_bus.stationId, my_bus.routeId, my_bus.staOrder);
+
+    myHTTP.begin(mySocket, buffer);
+
+    if(myHTTP.GET() == HTTP_CODE_OK)
+    {
+        printf("getBusArrivalItem \r\n");
+        strcpy(xmlDoc, myHTTP.getString().c_str());
+        printf("%s \r\n", xmlDoc);
+    }
+    else
+    {
+        printf("getBusArrivalItem error! \r\n");
+    }
+
+    XMLDocument xmlDocument;
+    if(xmlDocument.Parse(xmlDoc)!= XML_SUCCESS)
+    {
+        Serial.println("getBusArrivalItem error parsing");  
+        return;
+    };
+
+    XMLNode * root = xmlDocument.RootElement();
+    XMLElement * element1 = root->FirstChildElement("msgHeader")->FirstChildElement("busArrivalItem")->FirstChildElement("locationNo1");
+    XMLElement * element2 = root->FirstChildElement("msgHeader")->FirstChildElement("busArrivalItem")->FirstChildElement("predictTime1");
+    XMLElement * element3 = root->FirstChildElement("msgHeader")->FirstChildElement("busArrivalItem")->FirstChildElement("locationNo2");
+    XMLElement * element4 = root->FirstChildElement("msgHeader")->FirstChildElement("busArrivalItem")->FirstChildElement("predictTime2");
+
+    element1->QueryIntText(&my_bus.locationNo1);	
+    element2->QueryIntText(&my_bus.predictTime1);	
+    element3->QueryIntText(&my_bus.locationNo2);	
+    element4->QueryIntText(&my_bus.predictTime2);	
+
+    Serial.println(my_bus.locationNo1);
+    Serial.println(my_bus.predictTime1);
+    Serial.println(my_bus.locationNo2);
+    Serial.println(my_bus.predictTime2);
+
+    myHTTP.end();
+}
+
 
 void clearUserData()
 {
@@ -213,16 +419,29 @@ void setup() {
     Serial.println();
 
     resetUserData(0);
+
+    // Init and get the time
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+    getBusStationId();
+    getStaOrder();
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
     if(Serial.available())
     {
-        char reset = Serial.read();
+        char key = Serial.read();
 
-        if(reset == 'r')
+        if(key == 'r')
             resetUserData(1);
+        else if(key == 't')
+            getTime();
+        else if(key == 'w')
+            getWeather();
+        else if(key == 'b')
+            getBusArrival();
     }
 
     if(!checkWiFiStatus())
@@ -230,27 +449,4 @@ void loop() {
         Serial.println("WiFi Disconnected, Check WiFi Signal");
         resetUserData(1);
     }
-
-
-    delay(10);
-
-    // switch(my_state)
-    // {
-    //     case WIDGET_NULL:
-    //         neopixelWrite(RGB_BUILTIN,0xFF,0x00,0x00); // red 
-
-    //         break;
-    //     case WIDGET_STOP:
-    //         neopixelWrite(RGB_BUILTIN,0xFF,0xFF,0x00); // yellow
-
-    //         break;
-    //     case WIDGET_GO:
-    //         neopixelWrite(RGB_BUILTIN,0x00,0xFF,0x00); // green
-
-    //         break;
-
-    //     defalut:
-
-    //         break;
-    // }
 }
